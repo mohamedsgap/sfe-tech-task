4813b9f84da2472b4bc71540c7b1d04ae2e0fbef
Mohamed Abdelnasser Abdou <mmabdelnasser@gmail.com>
2025-07-20 21:31:16 +0200

    feat: implement route guards for protected routes

--- src/app/features/users/users.routes.ts
+++ src/app/features/users/users.routes.ts
@@ -1,18 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '../../core/guards/auth.guard';
 
 export const USERS_PATH = 'users'
 
 export const USERS_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./users-list-page/users-list-page.component').then(c => c.UsersListPageComponent)
+    loadComponent: () => import('./users-list-page/users-list-page.component').then(c => c.UsersListPageComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'create',
-    loadComponent: () => import('./user-form-page/user-form-page.component').then(c => c.UserFormPageComponent)
+    loadComponent: () => import('./user-form-page/user-form-page.component').then(c => c.UserFormPageComponent),
+    canActivate: [authGuard]
   },
   {
     path: ':id',
-    loadComponent: () => import('./user-form-page/user-form-page.component').then(c => c.UserFormPageComponent)
+    loadComponent: () => import('./user-form-page/user-form-page.component').then(c => c.UserFormPageComponent),
+    canActivate: [authGuard]
   }
 ]
