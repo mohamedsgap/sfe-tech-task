fa62873407a70e191012d6411f156ebb6bcff384
Mohamed Abdelnasser Abdou <mmabdelnasser@gmail.com>
2025-07-20 22:24:39 +0200

    feat: implement users list component

--- src/app/features/users/users-list-page/users-list-page.component.ts
+++ src/app/features/users/users-list-page/users-list-page.component.ts
@@ -1,21 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UsersListComponent } from '../users-list/users-list.component';
 import { MatButton } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { UsersFacadeService } from '../../../core/facades/users-facade.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users-list-page',
   imports: [
+    CommonModule,
     UsersListComponent,
     MatButton
   ],
   templateUrl: './users-list-page.component.html',
   styleUrl: './users-list-page.component.scss'
 })
-export class UsersListPageComponent {
+export class UsersListPageComponent implements OnInit {
   facade = inject(UsersFacadeService);
   router = inject(Router);
+  
+  ngOnInit(): void {
+    // Load users when the component initializes
+    this.loadUsers();
+  }
+  
+  loadUsers(): void {
+    this.facade.loadUsers();
+  }
 
   goToNew(): void {
     this.router.navigate(['/users/create']);
--- src/app/features/users/users-list/users-list.component.ts
+++ src/app/features/users/users-list/users-list.component.ts
@@ -1,9 +1,14 @@
 import { Component, input, InputSignal, output, OutputEmitterRef } from '@angular/core';
 import { User } from '../../../shared/models/user';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule
+  ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
